6224e5e68881906f30a72752c726f0a34d10d33a
Jose Pascarella <josepas@amazon.com>
2024-11-16 20:01:29 +0100

    Remove conflicts with Amplify imported CSS
    Signed-off-by: Jose Pascarella <josepas@amazon.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 "use client";
 
-import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
+import './globals.css';
 import Link from "next/link";
 
 Amplify.configure(outputs);
 
 export default function HomePage() {
   return (
-    <section className="home">
+    <section>
       <h1>Willkommen bei Physiotherapie Antonio Baptista</h1>
       <p>
         Ihre Gesundheit und Ihr Wohlbefinden sind unsere Priorität. Wir bieten
